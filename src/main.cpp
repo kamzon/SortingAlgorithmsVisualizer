cb63523ccbb7eb39b070cf3a01c90a0b2af60c06
NK <n_k>
2022-04-21 18:55:01 +0000

    sorting buble algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 
 int main() {
 
-    cout << " hello ansees" << endl;
+    std::vector<int> table = {12,88,71,3300,3301,3333,1,2,20,13};
+    int lenght = table.size();
+
+    for (int i=0; i<lenght; i++){
+        
+        for(int j=0; j<lenght-1; j++){
+            
+            if(table[j]>table[i]){
+                int temp = table[j];
+                table[j] = table[i];
+                table[i] = temp;
+                
+            }
+
+            
+            
+        }
+        for (int k=0;k<lenght ; k++){
+                cout << table[k] << " || ";
+            }
+            cout << endl;
+        
+    }
 
     return 0;
 }
